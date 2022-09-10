14a2611afd0098b5817adb784e92e611521e6e4c
cherPrado <cherlaufilho@discente.ufg.br>
2022-09-10 04:58:03 -0300

    Atualização do Menu

--- src/App.java
+++ src/App.java
@@ -1,34 +1,72 @@
-import java.util.ArrayList;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        ArrayList<Genre> genres = DatabaseManager.GetAllGenres();
-
-        for (Genre genre : genres) {
-            System.out.println(genre.toString());
-        }
-
         Menu menu = new Menu();
-        int p = 0;
+        int opcao = 0;
+        int continua = 2;
 
         do{
            try{
-                System.out.println(menu.principal());
-                p = sc.nextInt();
-
+                opcao = menu.principal(sc);
             }catch(RuntimeException e){
                 System.out.println(menu.erro());
-                break;
             }
 
-            switch(p){
-                case 1:
-                    System.out.println(menu.adicionar());
-                    p = sc.nextInt();
-                    switch(p){
+           
+            switch(opcao){
+            case 1:    
+                do{
+                    opcao = menu.adicionar(sc);
+                    switch(opcao){
+                        case 1: 
+                            continua = menu.addArtista(sc, sc);
+                            break;
+                        case 2: 
+                            continua = menu.addAlbum(sc, sc, sc);
+                            break;
+                        case 3: 
+                            continua = menu.addMusica(sc, sc, sc, sc);
+                            break;
+                        case 4: 
+                            // Gênero
+                            //DatabaseManager.AddGenre(name);
+                            break;
+                        default:
+                    }
+                }while(continua == 1);
+            break;
+        
+            case 2:
+            do{
+                opcao = menu.excluir(sc);
+                switch(opcao){
+                    case 1: 
+                       
+                        break;
+                    case 2: 
+                       
+                        break;
+                    case 3: 
+                      
+                        break;
+                    case 4: 
+                        // Gênero
+                        //DatabaseManager.AddGenre(name);
+                        break;
+                }
+
+            }while(continua == 1);
+            break;
+
+             /* 
+            case 3:
+                    System.out.println(menu.alterar());
+                    opcao = sc.nextInt();
+                    switch(opcao){
                         case 1: 
                             // Artista
                             break;
@@ -43,53 +81,30 @@ public class App {
                             break;
                     }
                     break;
-                case 2:
-                    System.out.println(menu.excluir());
-                    p = sc.nextInt();
-                    switch(p){
-                        case 1:
-                            break;
-                        case 2:
-                            break;
-                        case 3:
-                            break;
-                        case 4:
-                            break;
-                    }
-                    break;
-                case 3:
-                    System.out.println(menu.alterar());
-                    p = sc.nextInt();
-                    switch(p){
-                        case 1:
-                            break;
-                        case 2:
-                            break;
-                        case 3:
-                            break;
-                        case 4:
-                            break;
-                    }
-                    break;
                 case 4:
                     System.out.println(menu.consultar());
-                    p = sc.nextInt();
-                    switch(p){
-                        case 1:
+                    opcao = sc.nextInt();
+                    switch(opcao){
+                        case 1: 
+                            // Artista
                             break;
-                        case 2:
+                        case 2: 
+                            // Album
                             break;
-                        case 3:
+                        case 3: 
+                            // Música
                             break;
-                        case 4:
+                        case 4: 
+                            // Gênero
                             break;
                     }
                     break;
             }
-        
-        }while(false);
+        */
+        }
 
-
+         
         sc.close();
+    }while(continua != 3);
     }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,13 +1,17 @@
+
+import java.util.Scanner;
+
 public class Menu {
     
-    public String principal(){
-
-      return "\n Biblioteca de Músicas\n"
-             + "  (1) Adicionar\n"
-             + "  (2) Excluir  \n"
-             + "  (3) Alterar  \n"
-             + "  (4) Consultar\n"
-             + "  (5) Sair     \n";
+    public int principal(Scanner scanner){
+           System.out.println("\n Biblioteca de Músicas\n"
+                             + "  (1) Adicionar\n"
+                             + "  (2) Excluir  \n"
+                             + "  (3) Alterar  \n"
+                             + "  (4) Consultar\n"
+                             + "  (5) Sair     \n");
+        int opcao = scanner.nextInt();
+        return opcao;
     }
     
     public String opcoes(){
@@ -16,15 +20,22 @@ public class Menu {
                + "  (3) Música  \n"
                + "  (4) Gênero  \n";
     }
-    
-    public String adicionar(){
 
-        return "\n Adicionar" + opcoes();
+    
+    public int adicionar(Scanner sc){
+
+        System.out.println("\n Adicionar" + opcoes());
+        int opcao = sc.nextInt();
+        sc.nextLine();
+        return opcao;
     }
 
-    public String excluir(){
+    public int excluir(Scanner sc){
 
-        return "\n Excluir" + opcoes();
+        System.out.println("\n Excluir" + opcoes());
+        int opcao = sc.nextInt();
+        sc.nextLine();
+        return opcao;
     }
 
     public String alterar(){
@@ -40,4 +51,107 @@ public class Menu {
     public String erro(){
         return "Valor digitado não esta entre as opções";
     }
+    
+///////////////////////////////////////////////////////////////////////////////////////
+    public int addArtista(Scanner sc, Scanner w){
+
+        System.out.print(" Artista\n  -Nome do artista: ");
+        String nome; 
+        nome = sc.nextLine();
+        DatabaseManager.AddArtist(nome);
+        System.out.println("\n >Artista \"" + nome + "\" adicionado");
+
+        return voltarMenuAdicionar(w);
+    }
+
+    public int addAlbum(Scanner sc, Scanner t, Scanner w){
+
+        System.out.print(" Album\n -Nome do album: ");
+        String nome = sc.nextLine();
+        System.out.println("\n -O album \"" + nome +  "\" é de qual artista, informe o ID.\n");
+        System.out.println(" -Artistas: " + DatabaseManager.GetAllArtists());
+        System.out.printf("\n -ID do artista(int): ");
+        int id;
+        try{
+            id = t.nextInt();
+        }catch(RuntimeException e){
+            System.out.println("\n Valor invalido informe números inteiros.\n");
+            System.out.println(" -ID (do tipo int): ");
+            id = t.nextInt();
+        }
+        DatabaseManager.AddAlbum(nome, id);
+        System.out.println("\n >Album \"" + nome + "\" adicionado");
+
+        return voltarMenuAdicionar(w);
+    }
+
+    public int addMusica(Scanner sc, Scanner t, Scanner p, Scanner w){
+
+        System.out.print("\n Música\n -Nome da música: ");
+        String name = sc.nextLine();
+        System.out.println("\n -A música \"" + name +  "\" é de qual album, informe o ID.\n");
+        System.out.println(" -Albuns: " + DatabaseManager.GetAllAlbums());
+        int id;
+        try{
+            System.out.printf("\n -ID do Album(int): ");
+            id = t.nextInt();
+        }catch(RuntimeException e){
+            System.out.println("\nValor invalido informe números inteiros.\n");
+            System.out.println(" -ID (do tipo int): ");
+            id = t.nextInt();
+        }
+        int duration;
+        try{
+        System.out.printf("\n -Duração da música em segundos(int): ");
+        duration = p.nextInt();
+        }catch(RuntimeException e){
+            p.nextLine();
+            System.out.println("\n -Valor invalido informe números inteiros.\n");
+            System.out.print(" -Duração (do tipo int): ");
+            duration = p.nextInt();
+        }
+        DatabaseManager.AddTrack(name, id, duration);
+        System.out.println("\n >Música \"" + name + " \" adicionada< \n" );
+     
+        return voltarMenuAdicionar(w);
+    }
+    //////////////////////////////////////////////////////////////////////////////////////
+    /////// EXCLUIR.......
+
+
+
+
+    
+
+
+
+
+
+    public int voltarMenuPrincipal(Scanner w){
+        System.out.println("\n (1)Voltar ao menu principal\n" 
+                           + " (2)Fechar o programa\n");
+        int continua = w.nextInt();
+
+        if(continua == 1)
+            return 1;
+        else{
+            return 0;
+        }
+    }
+
+    public int voltarMenuAdicionar(Scanner w){
+        System.out.println("\n (1)Menu adicionar\n" 
+                           + " (2)Menu principal\n"
+                           + " (3)Fechar o programa");
+        int continua = w.nextInt();
+
+        if(continua == 1)
+            return 1;
+        else if(continua == 2){
+            return 2;
+        }else{
+            return 3;
+        }
+    }
+
 }
